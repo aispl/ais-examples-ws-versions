7f425ff6e0d13da183ba850476230a3fb46dec31
Tomasz Pik <tompik@gmail.com>
2014-03-07 08:12:32 +0100

    more on REST

--- src/main/java/pl/ais/examples/ws/versions/v2/services/Processor.java
+++ src/main/java/pl/ais/examples/ws/versions/v2/services/Processor.java
@@ -2,6 +2,11 @@ package pl.ais.examples.ws.versions.v2.services;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Service;
 
@@ -10,12 +15,18 @@ import pl.ais.examples.ws.versions.v2.Constants;
 import pl.ais.examples.ws.versions.v2.model.Request;
 import pl.ais.examples.ws.versions.v2.model.Response;
 
-@WebService(targetNamespace = Constants.NAMESPACE, name = "Processor")
+@WebService(targetNamespace = Constants.NAMESPACE, name = Processor.WEB_NAME)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path(Processor.WEB_NAME)
 @Service(Processor.NAME)
 public class Processor extends ProcessorService {
 
   public static final String NAME = "ProcessorV2";
+  public static final String WEB_NAME = "Processor";
 
+  @POST
+  @Path("process")
   @WebMethod(operationName = "process", action = Constants.NAMESPACE + "/process")
   public Response process(Request request) {
     Response response = new Response();
